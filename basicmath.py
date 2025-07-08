3a893f48e7427f1a28042c3cbfc4a6d916e20a8d
Hema Priya M <hemapriya.m@acuverconsulting.com>
2025-07-08 11:55:22 +0530

    Basic math operations

---
+++ basicmath.py
@@ -0,0 +1,13 @@
+a = 25
+b = 50
+c = 50
+d = "Basic math operations are done!!!"
+print (a+b) #addition
+print (b-c) #subtraction
+print (a*b) #multiplication
+print (b/a) #division
+if(a>b): #comparison
+    print ("a is greater than b")
+elif(b>a):
+    print ("b is greater than a")
+    print (d)
