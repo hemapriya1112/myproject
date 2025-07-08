333a591196bced1e751059b4d953f2975ba3006f
Hema Priya M <hemapriya.m@acuverconsulting.com>
2025-07-08 12:20:47 +0530

    basic math

--- basicmath.py
+++ basicmath.py
@@ -2,12 +2,12 @@ a = 25
 b = 50
 c = 50
 d = "Basic math operations are done!!!"
-print (a+b) #addition
-print (b-c) #subtraction
-print (a*b) #multiplication
-print (b/a) #division
-if(a>b): #comparison
-    print ("a is greater than b")
-elif(b>a):
-    print ("b is greater than a")
-    print (d)
+print("Addition of a and b is:", a + b)  # addition
+print("Subtraction of b and c is:", b - c)  # subtraction
+print("Multiplication of a and b is:", a * b)  # multiplication
+print("Dividend of a and b is:", b / a)  # division
+if (a > b):  # comparison
+    print("a is greater than b")
+elif (b > a):
+    print("b is greater than a")
+    print(d)
